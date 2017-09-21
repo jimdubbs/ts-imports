b0fdef31499eba82382733ea3dc18e1a0a2ec2cd
Jimmy Westcott <jimmywestcott@Jimmys-MacBook-Pro.local>
2017-09-21 10:50:39 -0230

    init

---
+++ lib/example.ts
@@ -0,0 +1,7 @@
+export class Example {
+    foo: string;
+
+    constructor(foo: string) {
+        this.foo = foo;
+    }
+}
---
+++ server/main.ts
@@ -0,0 +1,4 @@
+import { Example } from 'lib/example'
+
+let example = new Example('bar');
+let example2 = new Example('foo');
